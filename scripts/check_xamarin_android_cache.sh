75422e2226311c398b8c5c80942bcbdba63fac80
Eli Bishop <eli@launchdarkly.com>
2019-06-19 13:08:17 -0700

    fix path in if test

--- scripts/check_xamarin_android_cache.sh
+++ scripts/check_xamarin_android_cache.sh
@@ -2,7 +2,7 @@
 
 # used only for CI build
 
-if [ -e "~/project/xamarin.android-oss_v9.2.99.172_Linux-x86_64_master_d33bbd8e-Release" ]; then
+if [ -e "xamarin.android-oss_v9.2.99.172_Linux-x86_64_master_d33bbd8e-Release" ]; then
 	echo "Xamarin Android cache exists"
 else
 	wget https://jenkins.mono-project.com/view/Xamarin.Android/job/xamarin-android-linux/lastSuccessfulBuild/Azure/processDownloadRequest/xamarin-android/xamarin.android-oss_v9.2.99.172_Linux-x86_64_master_d33bbd8e-Release.tar.bz2
