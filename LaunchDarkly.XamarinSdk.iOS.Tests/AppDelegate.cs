d55c90a09f29e4c086581512d0e553cf43166aa0
Eli Bishop <eli@launchdarkly.com>
2019-06-24 16:39:00 -0700

    run iOS tests in CI

--- LaunchDarkly.XamarinSdk.iOS.Tests/AppDelegate.cs
+++ LaunchDarkly.XamarinSdk.iOS.Tests/AppDelegate.cs
@@ -8,7 +8,6 @@ using UIKit;
 using Xunit.Runner;
 using Xunit.Sdk;
 
-
 namespace LaunchDarkly.Xamarin.iOS.Tests
 {
     // The UIApplicationDelegate for the application. This class is responsible for launching the 
@@ -17,7 +16,6 @@ namespace LaunchDarkly.Xamarin.iOS.Tests
     [Register("AppDelegate")]
     public partial class AppDelegate : RunnerAppDelegate
     {
-
         //
         // This method is invoked when the application has loaded and is ready to run. In this 
         // method you should instantiate the window, load the UI into it and then make the window
@@ -29,7 +27,6 @@ namespace LaunchDarkly.Xamarin.iOS.Tests
         {
             // We need this to ensure the execution assembly is part of the app bundle
             AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
-            
 
             // tests can be inside the main assembly
             AddTestAssembly(Assembly.GetExecutingAssembly());
@@ -37,14 +34,13 @@ namespace LaunchDarkly.Xamarin.iOS.Tests
             // become part of the app bundle
             //AddTestAssembly(typeof(PortableTests).Assembly);
 
-#if false
 			// you can use the default or set your own custom writer (e.g. save to web site and tweet it ;-)
-			Writer = new TcpTextWriter ("10.0.1.2", 16384);
+			// Writer = new TcpTextWriter ("10.0.1.2", 16384);
 			// start running the test suites as soon as the application is loaded
 			AutoStart = true;
 			// crash the application (to ensure it's ended) and return to springboard
 			TerminateAfterExecution = true;
-#endif
+
             return base.FinishedLaunching(app, options);
 		}
     }
