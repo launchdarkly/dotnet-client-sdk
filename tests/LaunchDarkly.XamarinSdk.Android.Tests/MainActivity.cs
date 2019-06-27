0020505dacfb48199f8f42973e517551a8d9a35c
Eli Bishop <eli@launchdarkly.com>
2019-06-26 18:02:57 -0700

    add a new Android test project made from scratch

---
+++ tests/LaunchDarkly.XamarinSdk.Android.Tests/MainActivity.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+
+using Android.App;
+using Android.OS;
+using Xunit.Runners.UI;
+using Xunit.Sdk;
+
+namespace LaunchDarkly.Xamarin.Android.Tests
+{
+    [Activity(Label = "LaunchDarkly.Xamarin.Android.Tests", MainLauncher = true)]
+    public class MainActivity : RunnerActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            AddExecutionAssembly(typeof(ExtensibilityPointFactory).Assembly);
+            AddTestAssembly(Assembly.GetExecutingAssembly());
+
+            AutoStart = true;
+            //TerminateAfterExecution = true;
+
+            base.OnCreate(bundle);
+        }
+    }
+}
