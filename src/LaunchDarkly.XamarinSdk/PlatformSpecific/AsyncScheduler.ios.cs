184a0543e3db4db2a26746961c88eb328ecd8912
Eli Bishop <eli@launchdarkly.com>
2019-07-08 20:06:26 -0700

    indents

--- src/LaunchDarkly.XamarinSdk/PlatformSpecific/AsyncScheduler.ios.cs
+++ src/LaunchDarkly.XamarinSdk/PlatformSpecific/AsyncScheduler.ios.cs
@@ -3,11 +3,11 @@ using Foundation;
 
 namespace LaunchDarkly.Xamarin.PlatformSpecific
 {
-	internal static partial class AsyncScheduler
-	{
+    internal static partial class AsyncScheduler
+    {
         private static void PlatformScheduleAction(Action a)
-		{
+        {
             NSRunLoop.Main.BeginInvokeOnMainThread(a.Invoke);
         }
-	}
+    }
 }
