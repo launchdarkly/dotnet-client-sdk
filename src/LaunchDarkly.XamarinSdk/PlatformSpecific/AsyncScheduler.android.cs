001b98cd6793326aebf9054c46351cc7c9900a7f
Eli Bishop <eli@launchdarkly.com>
2019-07-09 12:43:51 -0700

    simpler Android AsyncScheduler implementation

--- src/LaunchDarkly.XamarinSdk/PlatformSpecific/AsyncScheduler.android.cs
+++ src/LaunchDarkly.XamarinSdk/PlatformSpecific/AsyncScheduler.android.cs
@@ -5,16 +5,14 @@ namespace LaunchDarkly.Xamarin.PlatformSpecific
 {
     internal static partial class AsyncScheduler
     {
-        private static Handler handler;
-
         private static void PlatformScheduleAction(Action a)
         {
-            // This is based on the Android implementation in Xamarin.Essentials:
-            // https://github.com/xamarin/Essentials/blob/master/Xamarin.Essentials/MainThread/MainThread.android.cs
-            if (handler?.Looper != Looper.MainLooper)
-            {
-                handler = new Handler(Looper.MainLooper);
-            }
+            // Note that this logic is different from the implementation of the equivalent method in Xamarin Essentials
+            // (https://github.com/xamarin/Essentials/blob/master/Xamarin.Essentials/MainThread/MainThread.android.cs);
+            // it creates a new Handler object each time rather than lazily creating a static one. This avoids a potential
+            // race condition, at the expense of creating more ephemeral objects. However, in our use case we do not
+            // expect this method to be called very frequently since we are using it for flag change listeners only.
+            var handler = new Handler(Looper.MainLooper);
             handler.Post(a);
         }
     }
