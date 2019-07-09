3a2a2e00fc366cc76c704dbc5a3f3db959d87fbe
Eli Bishop <eli@launchdarkly.com>
2019-07-08 20:03:59 -0700

    indents

--- src/LaunchDarkly.XamarinSdk/PlatformSpecific/AsyncScheduler.android.cs
+++ src/LaunchDarkly.XamarinSdk/PlatformSpecific/AsyncScheduler.android.cs
@@ -5,17 +5,17 @@ namespace LaunchDarkly.Xamarin.PlatformSpecific
 {
     internal static partial class AsyncScheduler
     {
-		private static Handler handler;
+        private static Handler handler;
 
-		private static void PlatformScheduleAction(Action a)
+        private static void PlatformScheduleAction(Action a)
         {
-			// This is based on the Android implementation in Xamarin.Essentials:
-			// https://github.com/xamarin/Essentials/blob/master/Xamarin.Essentials/MainThread/MainThread.android.cs
-			if (handler?.Looper != Looper.MainLooper)
-			{
-				handler = new Handler(Looper.MainLooper);
-			}
-			handler.Post(a);
-		}
-	}
+            // This is based on the Android implementation in Xamarin.Essentials:
+            // https://github.com/xamarin/Essentials/blob/master/Xamarin.Essentials/MainThread/MainThread.android.cs
+            if (handler?.Looper != Looper.MainLooper)
+            {
+                handler = new Handler(Looper.MainLooper);
+            }
+            handler.Post(a);
+        }
+    }
 }
