a17153e16a48a70d9c136247d0230f4429763654
torchhound <5600929+torchhound@users.noreply.github.com>
2019-08-21 15:28:36 -0700

    Added second value check inside Online, moved value check into try in SetOnlineAsync

--- src/LaunchDarkly.XamarinSdk/LdClient.cs
+++ src/LaunchDarkly.XamarinSdk/LdClient.cs
@@ -71,6 +71,10 @@ namespace LaunchDarkly.Xamarin
             get => _online;
             set
             {
+                if (value == _online)
+                {
+                    return;
+                }
                 AsyncUtils.WaitSafely(() => SetOnlineAsync(value));
             }
         }
@@ -284,13 +288,13 @@ namespace LaunchDarkly.Xamarin
         public async Task SetOnlineAsync(bool value)
         {
             await _connectionLock.WaitAsync();
-            if (value == _online)
-            {
-                return;
-            }
-            _online = value;
             try
             {
+                if (value == _online)
+                {
+                    return;
+                }
+                _online = value;
                 if (_online)
                 {
                     await RestartUpdateProcessorAsync(Config.PollingInterval);
