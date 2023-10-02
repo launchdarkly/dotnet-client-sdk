526a541963c10e085bec58e5f9d47373ab36afc7
Casey Waldren <cwaldren@launchdarkly.com>
2023-10-02 12:11:07 -0700

    propagate platform name in netstandard configuration

--- src/LaunchDarkly.ClientSdk/PlatformSpecific/DeviceInfo.netstandard.cs
+++ src/LaunchDarkly.ClientSdk/PlatformSpecific/DeviceInfo.netstandard.cs
@@ -1,3 +1,4 @@
+using System.Runtime.InteropServices;
 using LaunchDarkly.Sdk.EnvReporting;
 using LaunchDarkly.Sdk.EnvReporting.LayerModels;
 
@@ -5,7 +6,25 @@ namespace LaunchDarkly.Sdk.Client.PlatformSpecific
 {
     internal static partial class DeviceInfo
     {
-        private static IOptionalProp<OsInfo> PlatformGetOsInfo() => new Props.None<OsInfo>();
+        private static IOptionalProp<OsInfo> PlatformGetOsInfo()
+        {
+            var osName = "unknown";
+            var osFamily = "unknown";
+            var osVersion = "unknown";
+            
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+            {
+                osName = OSPlatform.Linux.ToString();
+            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+            {
+                osName = OSPlatform.Windows.ToString();
+            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
+            {
+                osName = OSPlatform.OSX.ToString();
+            }
+
+            return new Props.Some<OsInfo>(new OsInfo(osFamily, osName, osVersion));
+        }
 
         private static IOptionalProp<LaunchDarkly.Sdk.EnvReporting.LayerModels.DeviceInfo> PlatformGetDeviceInfo() =>
             new Props.None<EnvReporting.LayerModels.DeviceInfo>();
