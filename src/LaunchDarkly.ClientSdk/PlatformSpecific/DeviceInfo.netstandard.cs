cf6731bf3f52027dadc11e6f028a3773d03fe75f
Casey Waldren <cwaldren@launchdarkly.com>
2023-10-09 14:24:17 -0700

    return 'unknown' on all ld_device props on netstandard target

--- src/LaunchDarkly.ClientSdk/PlatformSpecific/DeviceInfo.netstandard.cs
+++ src/LaunchDarkly.ClientSdk/PlatformSpecific/DeviceInfo.netstandard.cs
@@ -1,30 +1,10 @@
-using System.Runtime.InteropServices;
-using LaunchDarkly.Sdk.EnvReporting;
 using LaunchDarkly.Sdk.EnvReporting.LayerModels;
 
 namespace LaunchDarkly.Sdk.Client.PlatformSpecific
 {
     internal static partial class DeviceInfo
     {
-        private static OsInfo? PlatformGetOsInfo()
-        {
-            var osName = "unknown";
-            var osFamily = "unknown";
-            var osVersion = "unknown";
-            
-            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
-            {
-                osName = OSPlatform.Linux.ToString();
-            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                osName = OSPlatform.Windows.ToString();
-            } else if (RuntimeInformation.IsOSPlatform(OSPlatform.OSX))
-            {
-                osName = OSPlatform.OSX.ToString();
-            }
-
-            return new OsInfo(osFamily, osName, osVersion);
-        }
+        private static OsInfo? PlatformGetOsInfo() => null;
 
         private static LaunchDarkly.Sdk.EnvReporting.LayerModels.DeviceInfo? PlatformGetDeviceInfo() => null;
     }
